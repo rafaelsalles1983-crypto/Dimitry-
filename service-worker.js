5c601f2c9d51a69d71aeaf439adfa8e346ea25c7
rafaelsalles1983-crypto <rafaelsalles1983@gmail.com>
2025-10-19 15:15:41 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "slavclub-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "chat.html",
+  "exercicio.html",
+  "jogo.html",
+  "complete-a-frase.html",
+  "script.js"
+];
+
+// Instalação do Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Busca do cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Atualização do cache
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
